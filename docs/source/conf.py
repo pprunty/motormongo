9d5ee84f8d33fef88527133625a0eb932b3f1286
pprunty <pprunty@tcd.ie>
2024-03-01 12:09:19 +0000

    update readthedocs

---
+++ docs/source/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'motormongo'
+copyright = '2024, Patrick Prunty'
+author = 'Patrick Prunty'
+release = '0.1.13'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+
+# Set the path to your package's modules
+import motormongo
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. motormongo documentation master file, created by
+   sphinx-quickstart on Fri Mar  1 11:26:29 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to motormongo's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
